f20be01b205d5fbf94e979418a055679e5d6f01f
jqluo <39048164+LuoPangpang@users.noreply.github.com>
2025-01-24 13:51:07 +0800

    Update example.run_affmb.py

--- example/example.run_affmb.py
+++ example/example.run_affmb.py
@@ -7,6 +7,6 @@ vdj_file = 'example.filtered_contig.annotation.csv'
 affmb.paired_repertoire_analysis(vdj_file,outdir,depth_filter=2)
 
 outdir = 'test_bulk'
-subprocess.call('mkdir -p '+outdir,shell==True)
+subprocess.call('mkdir -p '+outdir,shell=True)
 vdj_file = 'bulk_contig13m.annotation.csv'
 affmb.IGH_repertoire_analysis(vdj_file,outdir,depth_filter=2)
